e2011f9d826b1b0f172b2cb839183cdab19c3b4b
Milan Jolly <milan.cs16@iitp.ac.in>
2020-04-10 14:24:33 +0530

    Added dsl script for building a job

---
+++ runway/dsl.groovy
@@ -0,0 +1,14 @@
+def gitUrl = 'git://github.com/mijo2/devops_projects/tree/week-1'
+
+job('test-job') {
+    scm {
+        git(gitUrl)
+    }
+    triggers {
+        scm('30 14 * * *')
+    }
+    steps {
+        shell("cd week1_work")
+        maven('-e clean install')
+    }
+}

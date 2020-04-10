c99ea01ef452a1bba82fec86b0c887f2fc5230a8
Milan Jolly <milan.cs16@iitp.ac.in>
2020-04-10 15:35:49 +0530

    Updated information for git credentials and branch

--- runway/dsl.groovy
+++ runway/dsl.groovy
@@ -2,9 +2,13 @@ def gitUrl = 'git://github.com/mijo2/devops_projects/'
 
 job('test-job') {
     scm {
-        git(gitUrl)
-        credentialsId("GitHub")
-        branches("week-1")
+        git{
+            remote{
+                url(gitUrl)
+                credentials('GitHub')
+            }
+            branch('*/week-1')
+        }
     }
     triggers {
         scm('02 15 * * *')

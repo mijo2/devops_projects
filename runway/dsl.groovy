a374f7ce4ef8aaf75a78c218c3689dba766d1dca
Milan Jolly <milan.cs16@iitp.ac.in>
2020-04-10 16:53:01 +0530

    Updated the working driectory to a global variable

--- runway/dsl.groovy
+++ runway/dsl.groovy
@@ -1,4 +1,5 @@
 def gitUrl = 'https://github.com/mijo2/devops_projects/'
+def path_to_working = '/var/jenkins_home/workspace/test-job/week1_work'
 
 job('test-job') {
     scm {
@@ -14,7 +15,7 @@ job('test-job') {
         scm('* * * * *')
     }
     steps {
-        dir("/var/jenkins_home/workspace/test-job/week1_work"){
+        dir(path_to_working){
             sh "pwd"
         }
         maven('-e clean install')

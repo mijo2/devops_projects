e3cc1e1add761a99ac9b0462c4719bdef888c39a
Milan Jolly <milan.jolly18@gmail.com>
2020-05-09 03:01:45 +0530

    Updated a public attribute to private

--- Question.java
+++ Question.java
@@ -5,7 +5,7 @@ class Question{
     // Private variables
     private String question;
     private String opt1, opt2, opt3, opt4;
-    public String answer;
+    private String answer;
 
     public Question(String question, String opt1, String opt2, String opt3, String opt4, String answer){
         this.question = question;
